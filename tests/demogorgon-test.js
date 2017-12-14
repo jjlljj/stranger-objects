ccaad014af45462f71b9abad1cfe6e2e6d8d3bac
James Logue <logueja@gmail.com>
2017-12-14 11:15:43 -0700

    Intial commit

---
+++ tests/demogorgon-test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+var Demagorgon = require('../lib/demogorgon.js');
+
+describe('Demagorgon', function () {
+
+  it('should be a function', function () {
+    assert.isFunction(Demagorgon);
+  });
+
+});
